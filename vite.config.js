72885ff1c6518e22641c7afe5a25ba3e70d12336
Allen <allenyu0708@gmail.com>
2022-08-08 23:15:56 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test123/',
+  // base: '/test123/',
   plugins: [vue()],
   resolve: {
     alias: {
